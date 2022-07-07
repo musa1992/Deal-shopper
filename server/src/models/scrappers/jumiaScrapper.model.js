c4ac6bdc2b4f15ae8053fcc962432b1f9107ca3b
musa1992 <mosesbwire91@gmail.com>
2022-07-07 13:03:20 +0300

    adds functionality to extract category links

--- server/src/models/scrappers/jumiaScrapper.model.js
+++ server/src/models/scrappers/jumiaScrapper.model.js
@@ -1,85 +1,50 @@
 const puppeteer = require('puppeteer');
 
-
-
-// async function loadPage(page, url){
-//     await page.goto(url,{waitUntil: 'load', timeout: 0})   
-// }
-
-// async function getElements(page){
-   
-//     const element = await page.waitForSelector('.flyout', {timeout: 0})  
-
-//     const categoriesLinkArray = await element.$$eval('.itm', nodes => nodes.map(node => node.href))
-    
-//     console.log(categoriesLinkArray)
-    
-//     return categoriesLinkArray
-
-// }
-
-// async function run (){
-//     const url = 'https://www.jumia.co.ke'
-//     const browser = await puppeteer.launch({headless: false})
-//     const page = await browser.newPage();
-//     await loadPage(page,url)
-
-//     const categoryLinks = await getElements(page)
-
-//     await page.goto(categoryLinks[0])
-
-//     await browser.close()
-// }
-
 const JumiaScrapper = ()=>{
-    const primaryUrl = 'https://www.jumia.co.ke/';
-    let searchItems;
-    let rawData;
-
-    function isCorrectArgument(input){
-       
-        return input.every(element => element.category && element.name && element.quantity ) 
-
-    }
-
-    function setSearchItems(shoppingItems){
-        if(isCorrectArgument(shoppingItems)){
-            searchItems = shoppingItems
-        }else{
-            //create error object
-            throw 'Item in the list is not categorized'
-        }
-    }
-
-    function getSearchItems(){
-        return searchItems
-    }
-
+    const url = 'https://www.jumia.co.ke/groceries';
+    let categoryLinks;
 
     async function launchScrapper(){
         const browser = await puppeteer.launch({headless:false})
         const page = await browser.newPage();
         
-        await page.goto('https://www.jumia.co.ke/sugar-flour',{waitUntil: 'load', timeout: 0})
+        await page.goto(url,{waitUntil: 'load', timeout: 0})
+        
 
-        rawData = await page.evaluate(()=>{
-            const data = []
-            const items = document.querySelectorAll('.prd')
+        categoryLinks = await page.evaluate(()=>{
+            
+            const container = document.querySelectorAll('.ttl')
 
-            for (const item of items){
-                data.push({
-                    name: item.querySelector('.name').innerHTML,
-                    price: item.querySelector('.prc').innerHTML
-                })
+            let categories;
+            let links = []
+
+            for(const item of container){
+                if( item.innerHTML === 'Shop by Category'){
+                    categories = item.nextSibling.querySelectorAll('.col')
+                }
             }
-            return data
-        })  
 
+            for (const category of categories){
+                links.push(category.href)
+            }
+            
+            // const data = []
+            // const items = document.querySelectorAll('.prd')
+
+            // for (const item of items){
+            //     data.push({
+            //         name: item.querySelector('.name').innerHTML,
+            //         price: item.querySelector('.prc').innerHTML
+            //     })
+            // }
+            // return data
+            return links
+        })  
 
         await browser.close()
     }
     
-    return {setSearchItems, launchScrapper}
+    return {launchScrapper}
 }
 
 const jumiaScrapper = JumiaScrapper()
